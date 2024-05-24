78896b3f52103c7fe883e895d5da29eb55c3c6be
Movash <andrushhenko97@gmail.com>
2024-05-24 23:40:35 +0300

    add_more_styles

--- src/components/QuizList/QuizList.tsx
+++ src/components/QuizList/QuizList.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import { FC } from 'react';
 import { Quiz } from '../QuizManager/QuizManager';
 
 interface QuizListProps {
   quizzes: Quiz[];
   editQuiz: (quiz: Quiz) => void;
-  deleteQuiz: (quizId: number) => void;
+  deleteQuiz: (quizId: string) => void;
 }
 
-export const QuizList: React.FC<QuizListProps> = ({
+export const QuizList: FC<QuizListProps> = ({
   quizzes,
   editQuiz,
   deleteQuiz,
@@ -17,10 +17,16 @@ export const QuizList: React.FC<QuizListProps> = ({
       {quizzes.map(quiz => (
         <div key={quiz.id} className="quiz-item">
           <h2>{quiz.title}</h2>
-          <button onClick={() => editQuiz(quiz)} className="btn">
+          <button
+            onClick={() => editQuiz(quiz)}
+            className="font-medium h-12 w-24 bg-amber-500 text-white rounded-lg transition duration-300 ease-in-out transform hover:bg-amber-700"
+          >
             Edit
           </button>
-          <button onClick={() => deleteQuiz(quiz.id)} className="btn">
+          <button
+            onClick={() => deleteQuiz(quiz.id)}
+            className="font-medium h-12 w-24 bg-red-500 text-white rounded-lg transition duration-300 ease-in-out transform hover:bg-red-700"
+          >
             Delete
           </button>
         </div>
