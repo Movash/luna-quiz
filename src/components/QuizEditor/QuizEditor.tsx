19358e3f037e9907e23bc43558aa71eb8674460b
Movash <andrushhenko97@gmail.com>
2024-05-24 23:40:18 +0300

    use_nanoid_and_add_more_styles

--- src/components/QuizEditor/QuizEditor.tsx
+++ src/components/QuizEditor/QuizEditor.tsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
+import { ChangeEvent, FC, useState } from 'react';
 import { Quiz, Question } from '../QuizManager/QuizManager';
 import { QuestionEditor } from '../QuestionEditor/QuestionEditor';
+import { nanoid } from 'nanoid';
 
 interface QuizEditorProps {
   quiz: Quiz;
   saveQuiz: (quiz: Quiz) => void;
 }
 
-export const QuizEditor: React.FC<QuizEditorProps> = ({ quiz, saveQuiz }) => {
+export const QuizEditor: FC<QuizEditorProps> = ({ quiz, saveQuiz }) => {
   const [currentQuiz, setCurrentQuiz] = useState<Quiz>(quiz);
 
-  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setCurrentQuiz({ ...currentQuiz, title: e.target.value });
   };
 
@@ -23,9 +24,9 @@ export const QuizEditor: React.FC<QuizEditorProps> = ({ quiz, saveQuiz }) => {
 
   const addQuestion = () => {
     const newQuestion: Question = {
-      id: Date.now(),
+      id: nanoid(),
       questionText: '',
-      answers: [''],
+      answers: ['', ''],
       correctAnswerIndex: 0,
     };
     setCurrentQuiz({
@@ -34,7 +35,7 @@ export const QuizEditor: React.FC<QuizEditorProps> = ({ quiz, saveQuiz }) => {
     });
   };
 
-  const removeQuestion = (id: number) => {
+  const removeQuestion = (id: string) => {
     const updatedQuestions = currentQuiz.questions.filter(q => q.id !== id);
     setCurrentQuiz({ ...currentQuiz, questions: updatedQuestions });
   };
@@ -44,30 +45,41 @@ export const QuizEditor: React.FC<QuizEditorProps> = ({ quiz, saveQuiz }) => {
   };
 
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
       <input
         type="text"
         value={currentQuiz.title}
         onChange={handleTitleChange}
-        placeholder="Quiz Title"
-        className="input"
+        placeholder="Quiz title"
+        className=" mb-8 h-10 block w-60 px-3 py-2 border border-gray-300 rounded-md shadow-sm transition focus:outline-none focus:ring-blue-500 focus:border-blue-500 hover:border-blue-400 cursor-pointer"
       />
-      <button onClick={addQuestion} className="btn">
-        Add Question
-      </button>
-      {currentQuiz.questions.map((question, index) => (
-        <QuestionEditor
-          key={question.id}
-          question={question}
-          onChange={updatedQuestion =>
-            handleQuestionChange(index, updatedQuestion)
-          }
-          onDelete={() => removeQuestion(question.id)}
-        />
-      ))}
-      <button onClick={save} className="btn">
-        Save Quiz
-      </button>
+      <ul className="flex flex-wrap justify-center items-start gap-8 mb-8 w-full">
+        {currentQuiz.questions.map((question, index) => (
+          <QuestionEditor
+            key={question.id}
+            question={question}
+            questionNumber={index + 1}
+            onChange={updatedQuestion =>
+              handleQuestionChange(index, updatedQuestion)
+            }
+            onDelete={() => removeQuestion(question.id)}
+          />
+        ))}
+      </ul>
+      <div className="flex justify-center items-center gap-4">
+        <button
+          onClick={addQuestion}
+          className="font-medium h-12 w-60 bg-orange-400 text-white rounded-lg transition duration-300 ease-in-out transform hover:bg-orange-500"
+        >
+          Add new question
+        </button>
+        <button
+          onClick={save}
+          className="font-medium h-12 w-28 bg-blue-500 text-white rounded-lg transition duration-300 ease-in-out transform hover:bg-blue-700"
+        >
+          Save quiz
+        </button>
+      </div>
     </div>
   );
 };
