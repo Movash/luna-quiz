c3fb128b5e792e9543e83f4489295997d091dc0a
Movash <andrushhenko97@gmail.com>
2024-05-28 00:31:30 +0300

    add_promise_delay

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -2,12 +2,16 @@ import { Quiz } from '../components/QuizManager/QuizManager';
 
 const QUIZZES_KEY = 'quizzes';
 
-export const getQuizzes = (): Quiz[] => {
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+export const getQuizzes = async (): Promise<Quiz[]> => {
+  await delay(500);
   const quizzesState = localStorage.getItem(QUIZZES_KEY);
   return quizzesState ? JSON.parse(quizzesState) : [];
 };
 
-export const saveQuizzes = (quizzes: Quiz[]): void => {
+export const saveQuizzes = async (quizzes: Quiz[]): Promise<void> => {
+  await delay(500);
   const quizzesState = JSON.stringify(quizzes);
   localStorage.setItem(QUIZZES_KEY, quizzesState);
 };
