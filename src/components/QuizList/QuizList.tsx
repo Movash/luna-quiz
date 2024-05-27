d4e1a88a444fa292ebc2b743887766660262edda
Movash <andrushhenko97@gmail.com>
2024-05-27 21:14:43 +0300

    fix_markup_and_styles

--- src/components/QuizList/QuizList.tsx
+++ src/components/QuizList/QuizList.tsx
@@ -15,28 +15,33 @@ export const QuizList: FC<QuizListProps> = ({
   runQuiz,
 }) => {
   return (
-    <div>
+    <div className='flex flex-wrap gap-4'>
       {quizzes.map(quiz => (
-        <div key={quiz.id} className="quiz-item">
-          <h2>{quiz.title}</h2>
-          <button
-            onClick={() => editQuiz(quiz)}
-            className="font-medium h-12 w-24 bg-amber-500 text-white rounded-lg transition duration-300 ease-in-out transform hover:bg-amber-700"
-          >
-            Edit
-          </button>
-          <button
-            onClick={() => deleteQuiz(quiz.id)}
-            className="font-medium h-12 w-24 bg-red-500 text-white rounded-lg transition duration-300 ease-in-out transform hover:bg-red-700"
-          >
-            Delete
-          </button>
+        <div
+          key={quiz.id}
+          className="mx-auto p-4 border rounded-xl bg-slate-100 w-[300px]"
+        >
+          <h2 className="text-xl font-bold mb-4">{quiz.title}</h2>
           <button
             onClick={() => runQuiz(quiz)}
-            className="font-medium h-12 w-24 bg-blue-500 text-white rounded-lg transition duration-300 ease-in-out transform hover:bg-blue-700"
+            className="mb-2 font-medium h-12 w-full bg-blue-500 text-white rounded-lg transition duration-300 ease-in-out transform hover:bg-blue-700"
           >
             Start Quiz
           </button>
+          <div className="flex gap-2 justify-center">
+            <button
+              onClick={() => editQuiz(quiz)}
+              className="font-medium h-12 w-full bg-amber-500 text-white rounded-lg transition duration-300 ease-in-out transform hover:bg-amber-700"
+            >
+              Edit
+            </button>
+            <button
+              onClick={() => deleteQuiz(quiz.id)}
+              className="font-medium h-12 w-full bg-red-500 text-white rounded-lg transition duration-300 ease-in-out transform hover:bg-red-700"
+            >
+              Delete
+            </button>
+          </div>
         </div>
       ))}
     </div>
