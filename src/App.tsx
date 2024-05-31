f027ab14550f959527193741c7b27c5daba49057
Movash <andrushhenko97@gmail.com>
2024-05-31 22:31:02 +0300

    add_container

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { QuizManager } from './components/QuizManager/QuizManager';
 
 export const App: FC = () => {
   return (
-    <div className="w-[1440px] mx-auto p-8 text-center">
+    <div className="container mx-auto p-8 text-center">
       <h1 className="text-2xl font-bold mb-8">Luna Quiz</h1>
       <QuizManager />
     </div>
