ebf80efa702e58cbe6daefd6e41896fd826ae9c4
Movash <andrushhenko97@gmail.com>
2024-06-02 19:29:35 +0300

    add_back_to_main_button

--- src/components/QuizEditor/QuizEditor.tsx
+++ src/components/QuizEditor/QuizEditor.tsx
@@ -6,9 +6,10 @@ import { nanoid } from 'nanoid';
 interface QuizEditorProps {
   quiz: Quiz;
   saveQuiz: (quiz: Quiz) => void;
+  onBackToMain: () => void;
 }
 
-export const QuizEditor: FC<QuizEditorProps> = ({ quiz, saveQuiz }) => {
+export const QuizEditor: FC<QuizEditorProps> = ({ quiz, saveQuiz, onBackToMain }) => {
   const [currentQuiz, setCurrentQuiz] = useState<Quiz>(quiz);
 
   const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -45,46 +46,56 @@ export const QuizEditor: FC<QuizEditorProps> = ({ quiz, saveQuiz }) => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <input
-        type="text"
-        value={currentQuiz.title}
-        onChange={handleTitleChange}
-        placeholder="Quiz title"
-        className=" mb-8 h-10 block w-60 px-3 py-2 border border-gray-300 rounded-md shadow-sm transition focus:outline-none focus:ring-blue-500 focus:border-blue-500 hover:border-blue-400 cursor-pointer"
-      />
-      <ul className="flex flex-wrap justify-center items-start gap-8 mb-8 w-full">
-        {currentQuiz.questions.map((question, index) => (
-          <QuestionEditor
-            key={question.id}
-            question={question}
-            questionNumber={index + 1}
-            onChange={updatedQuestion =>
-              handleQuestionChange(index, updatedQuestion)
-            }
-            onDelete={() => removeQuestion(question.id)}
-          />
-        ))}
-      </ul>
-      <div className="flex justify-center items-center gap-4">
-        <button
-          onClick={addQuestion}
-          className="font-medium h-12 w-60 bg-orange-400 text-white rounded-lg transition duration-300 ease-in-out transform hover:bg-orange-500"
-        >
-          Add new question
-        </button>
-        <button
-          onClick={save}
-          disabled={!currentQuiz.title || currentQuiz.questions.length < 1}
-          className={`font-medium h-12 w-28 text-white rounded-lg transition duration-300 ease-in-out transform ${
-            !currentQuiz.title || currentQuiz.questions.length < 1
-              ? 'bg-gray-400 cursor-not-allowed'
-              : 'bg-blue-500 hover:bg-blue-700'
-          }`}
-        >
-          Save quiz
-        </button>
+    <>
+      <button
+        onClick={onBackToMain}
+        className="mb-8 font-medium h-12 w-48 bg-blue-500 text-white rounded-lg transition duration-300 ease-in-out transform hover:bg-blue-700"
+      >
+        Back to main
+      </button>
+      <div className="flex flex-col justify-center items-center">
+        <input
+          type="text"
+          value={currentQuiz.title}
+          onChange={handleTitleChange}
+          placeholder="Quiz title"
+          className=" mb-8 h-10 block w-60 px-3 py-2 border border-gray-300 rounded-md shadow-sm transition focus:outline-none focus:ring-blue-500 focus:border-blue-500 hover:border-blue-400 cursor-pointer"
+        />
+        {currentQuiz.questions.length > 0 && (
+          <ul className="flex flex-wrap justify-center items-start gap-8 mb-8 w-full">
+            {currentQuiz.questions.map((question, index) => (
+              <QuestionEditor
+                key={question.id}
+                question={question}
+                questionNumber={index + 1}
+                onChange={updatedQuestion =>
+                  handleQuestionChange(index, updatedQuestion)
+                }
+                onDelete={() => removeQuestion(question.id)}
+              />
+            ))}
+          </ul>
+        )}
+        <div className="flex justify-center items-center gap-4">
+          <button
+            onClick={addQuestion}
+            className="font-medium h-12 w-60 bg-orange-400 text-white rounded-lg transition duration-300 ease-in-out transform hover:bg-orange-500"
+          >
+            Add new question
+          </button>
+          <button
+            onClick={save}
+            disabled={!currentQuiz.title || currentQuiz.questions.length < 1}
+            className={`font-medium h-12 w-28 text-white rounded-lg transition duration-300 ease-in-out transform ${
+              !currentQuiz.title || currentQuiz.questions.length < 1
+                ? 'bg-gray-400 cursor-not-allowed'
+                : 'bg-blue-500 hover:bg-blue-700'
+            }`}
+          >
+            Save quiz
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
